40c8d872456b9129757a786f35afc04939c134b4
Pankaj Jaswal <57428746+PankajJaswalDrish@users.noreply.github.com>
2024-07-11 11:22:57 +0000

    Created a Application of Calcultor using basic Operators

---
+++ Exercise1_Calculator.py
@@ -0,0 +1,45 @@
+#Designed a Calculator with fix input values using different operators
+print("\t \t \t \t Welcome to MY Calculator Screen")
+#Both a and b are of type int
+a = 15
+b = 3
+print("\t \t \t \t \t \t Value of a = ", a)
+print("\t \t \t \t \t \t Value of b = ", b)
+print("\n")
+#Confirmation the date type of Variable
+print("Variable a Data type")
+print("\n")
+print("\t \t \t \t Variable a type", type(a))
+print("\t \t \t \t Variable b type", type(b))
+print("\n")
+
+#Operation on Variable a and b
+print("Opeartion on Variable a and b")
+print("\n")
+print("\t \t \t \t Addition of a and b  = ", a + b)
+print("\t \t \t \t Subtraction of a and b  = ", a - b)
+print("\t \t \t \t Multiplication of a and b  = ", a * b)
+print("\t \t \t \t Division of a and b  =  ", a / b)
+print("\t \t \t \t Floor Division of a and b  = ", a // b)
+print("\t \t \t \t Modulus of a and b  = ", a % b)
+print("\t \t \t \t",a, "to the power", b, "is", a**b)
+print("\n")
+#Operation on Variable a
+print("Operation on Variable a")
+print("\n")
+print("\t \t \t \t Squre of a  = ", a**2)
+print("\t \t \t \t cube of a  = ", a**3)
+print("\t \t \t \t Squre root of a  = ", a**0.5)
+print("\t \t \t \t Cube root of a  = ", a**0.33)
+print("\t \t \t \t a to the power 4  = ", a**4)
+print("\n")
+
+#Operation on Variable b
+print("Operration on Variable b")
+print("\n")
+print("\t \t \t \t Squre of b  = ", b**2)
+print("\t \t \t \t cube of b  = ", b**3)
+print("\t \t \t \t Squre root of b  = ", b**0.5)
+print("\t \t \t \t Cube root of b  = ", b**0.33)
+print("\t \t \t \t a to the power 4  = ", b**4)
+print("\n")
---
+++ operators.py
@@ -0,0 +1,12 @@
+'''Opeartors in python'''
+print("Addition : ", 5 + 6)  #Addition
+print("Subtraction : ", 5 - 5)  #Subtraction
+print("Multiplication : ", 5 * 5)  # * is used for multiplication
+print("Division : ", 5 / 5)  #division will always return float value
+print(
+    "Floor Division : ", 5 // 5
+)  #considers only the integer part of the division and is used to remove the decimal part
+print("Modulus or remainder : ", 5 % 5)  #considers only the remainder
+print("Modulus or remainder : ",
+      5**3)  #exponential operator is used to calculate the power of a number
+#it is used to calculate the power of a number
