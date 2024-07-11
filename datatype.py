8b6f22a3f2a51cd0440f825825ab28fb066d7fc3
Pankaj Jaswal <57428746+PankajJaswalDrish@users.noreply.github.com>
2024-07-11 10:31:43 +0000

    DataType in python

---
+++ datatype.py
@@ -0,0 +1,27 @@
+#basic data types
+a = 15
+print(a)
+b = "Pankaj"
+print(b)
+c = 15
+print(a + c)
+print("type of a and c is ", type(a), type(c))
+print("type of b is ", type(b))
+d = complex(8, 2)
+print("type of d is ", type(d))
+e = True
+print("type of e is ", type(e))
+
+#it is mutable means it can be changed
+list = [8, 2.3, [-4, 5], ["apple", "banana"]]
+print(list)
+
+#it is immutable means it cannot be changed
+tuple = (("parrot", "sparrow"), ("lion", "tiger"))
+print(tuple)
+
+#dictionaries are key value pairs
+dict = {"name": "Pankaj", "age": 20, "canVote": True}
+print(dict)
+
+# in python everthing is a object
