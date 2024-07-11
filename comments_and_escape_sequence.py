cda5e43bbee7918728299efc95e1c436c3307589
Pankaj Jaswal <57428746+PankajJaswalDrish@users.noreply.github.com>
2024-07-11 06:51:47 +0000

    Comment and Escape Sequence in Python

---
+++ comments_and_escape_sequence.py
@@ -0,0 +1,20 @@
+# Single Line comment 
+#Escape Sequence Characters \n used for new line
+#Escape Sequence Characters \" used for double quotes
+# print("Hello World!\nHello World!\nHello World!")
+'''
+  Multiline comment can we done using 
+'''
+
+"""
+Multiline comment can we done using 
+"""
+
+print("I LOVE MY COUNTRY \n My Country is very beautiful")
+print("I LOVE MY COUNTRY")
+
+
+# print different values in print statement 
+# ~(Separator)is used to join the character 
+# end="009" will be added at the end of the print statement.
+print("Hello", 5, 6, "Bye bye", sep="~", end="009\n")
